48c47d09a1283dfbb22aea49356de0b173864b6d
Matthew Forrester <matthew.forrester@speechmarks.com>
2015-08-05 08:10:23 +0100

    starting to build model for grid

--- interpreter.js
+++ interpreter.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var R = require('require-parts')('ramda', 'src', ['keys','countBy', 'mapObjIndexed', 'values', 'pipe', 'sort', 'map']);
+
 var getTLIdEncoderDecoder = require("get_tlid_encoder_decoder");
 
 var tLIdEncoderDecoder = getTLIdEncoderDecoder(new Date(2015, 6, 29).getTime(), 5);
@@ -8,18 +10,52 @@ function Intr() {
     this._d = {};
 }
 
-Intr._keyFromTime = function(l, d) {
-    return l + '-' + tLIdEncoderDecoder.encode(d);
+Intr.encode = function(l) {
+    return tLIdEncoderDecoder.encode(l.getTime());
 }
 
-Intr._timeFromKey = function(k) {
-    tLIdEncoderDecoder.decode(k.substr(2));
+Intr.decode = function(k) {
+    return new Date(tLIdEncoderDecoder.decode(k));
 }
 
 Intr.prototype.push = function(id, data) {
+    this._d[id] = data;
 };
 
+Intr._getConstructionMap = R.pipe(
+    R.keys,
+    R.countBy(function(s) { return s.substr(2) }),
+    R.mapObjIndexed(function(v, k) { return [k, v > 1]; }),
+    R.values,
+    R.sort(function(a, b) {
+        return tLIdEncoderDecoder.sort(b[0], a[0]);
+    })
+);
+
 Intr.prototype.get = function(id, data) {
+    var r = [],
+        consMap = Intr._getConstructionMap(this._d),
+        tmp = {},
+        i = 0;
+
+    var mapper = function(m) {
+        var dk = 'c-' + m[0];
+        var rr = {
+            "Connect Time": Intr.decode(m[0]).toISOString(),
+            "Response Time": null,
+            "Request Length": null,
+            "Request Headers": this._d[dk].headers,
+            "Method": this._d[dk].method,
+            "Request Body": this._d[dk].body,
+            "Target": this._d[dk].target,
+            "Status": "On Going"
+        };
+        if (m[i][1]) {
+        }
+        return rr;
+    }.bind(this);
+
+    return R.map(mapper, consMap);
 };
 
 module.exports = Intr;
--- test/interpreter.js
+++ test/interpreter.js
@@ -3,24 +3,41 @@
 var expect = require('expect.js'),
     Intr = require('../interpreter.js');
 
-var sample1 = {
-    person: { name: null },
-    order: { owner: { link: "person" } },
-    a: { letter: { link: "person.name" } },
-    b: { something: null }
-};
-
 
 var times = [];
 
 for (var i = 0; i < 100; i++) {
-    times.push(new Date(2014, 2, 2, 2, 2, i));
+    times.push(Intr.encode(new Date(2014, 2, 2, 2, 2, i)));
 }
+
+describe('interpreter', function() {
+    it('can prepare', function() {
+        var ks = [
+                'c-' + times[0],
+                'c-' + times[5],
+                'd-' + times[5],
+                'c-' + times[2],
+                'd-' + times[0],
+                'd-' + times[2],
+                'c-' + times[6]
+            ],
+            ob = {};
+        for (var i = 0; i < ks.length; i++) {
+            ob[ks[i]] = null;
+        }
+        expect(Intr._getConstructionMap(ob)).to.eql([
+            [times[0], true],
+            [times[2], true],
+            [times[5], true],
+            [times[6], false]
+        ]);
+    });
+});
 
 describe('interpreter', function() {
     it('can add remove add add remove remove and grid does whats expected', function() {
         var intr = new Intr();
-        intr.push(times[0], {
+        intr.push('c-' + times[0], {
             headers: { path: '/' },
             method: 'GET',
             body: 'a=1&b=2',
@@ -28,16 +45,18 @@ describe('interpreter', function() {
         });
         expect(intr.get()).to.eql([
             {
-                "Connect Time": times[0].toISOString(),
-                "Response Time": times[1].toISOString(),
-                "Request Length": 1000,
-                "Request Headers": '{"path":"/"}',
+                "Connect Time": Intr.decode(times[0]).toISOString(),
+                "Response Time": null,
+                "Request Length": null,
+                "Request Headers": { path: "/" },
                 "Method": "GET",
                 "Request Body": 'a=1&b=2',
                 "Target": 'web',
                 "Status": "On Going"
             }
         ]);
+                // "Response Time": times[1].toISOString(),
+                // "Request Length": 1000,
     });
 });
 
